31a7c603f26ea88d8b37ef5a215797d94be911c6
Fabrizio Avantaggiato <avafab@gmail.com>
2020-07-23 13:34:09 +0200

    added more questions

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ let games = [
     'Write a fragment of a sentence in the chat room. Now ask others to complete it.',
     'Describe your current mood in one word',
     'Tell us a story about something you’re wearing',
+    'Imitate your own professional meeting voice.',
+    'Sometimes I pretend to know how to [insert skill.]',
     //Questions
     'Where would you travel if you knew there was a chance you would have to stay there for up to a year?',
     'If you had to choose one car to drive forever, what would you pick?',
@@ -29,6 +31,29 @@ let games = [
     'Would you be willing to fight in one of the past wars?',
     'If you could have credit for any invention in history, what would you choose?',
     'If you were a refrigerator, what item would you hate holding?',
+    'What’s the strangest thing you’ve ever been asked to do during a meeting?',
+    'What did your 5th grade teacher say about you during your parent-teacher conference?',
+    'What was the silliest thing you or someone else has ever said during a meeting?',
+    'What is your first memory speaking in front of a group?',
+    'When was the last time you felt like you were “in trouble”?',
+    'What would your name be if your first name was your father’s middle name and your last name was your childhood favorite food?',
+    'If you had to replace your legs with the legs of an animal, what would you choose?',
+    'What do you think this meeting is about?',
+    'What’s something you would change if you ran the world?',
+    'What talent would you show off in a talent show?',
+    'What three hashtags describe your work persona? What three hashtags describe your social persona?',
+    'What’s your campaign slogan if you’re running for public office?',
+    'What is your most used emoji?',
+    'What is one place you never ever want to go?',
+    'If you could pick up a new skill in an instant what would it be?',
+    'What is the most embarrassing thing you accidentally messaged to someone?',
+    'What world problem do you think you have the solution to?',
+    'Do you find time passing differently when you work from home vs. when you work in the office?',
+    'Do you identify more with coffee or tea? Why?',
+    'Are there any shows you pretend you don’t watch when people ask you?',
+    'Do you ever think your parents gave you the wrong name?',
+    'What do you think about when you’re driving?',
+
 ];
 
 btn.addEventListener('click', function(){
